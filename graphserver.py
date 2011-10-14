098ddd3dff4266b2da518cebb065f36500ee2c5d
Lukas Blakk <lukasblakk@gmail.com>
2011-10-14 14:11:15 -0700

    took out the auth stuff since this will live behind LDAP & build-vpn

--- graphserver.py
+++ graphserver.py
@@ -29,8 +29,6 @@ def show_entries():
 
 @app.route('/branches', methods=['GET', 'POST'])
 def branches():
-    if not session.get('logged_in'):
-        abort(401)
     # TODO - form validation here
     con = engine.connect()
     if request.method == 'POST':
@@ -46,8 +44,6 @@ def branches():
 
 @app.route('/machines', methods=['GET', 'POST'])
 def machines():
-    if not session.get('logged_in'):
-        abort(401)
     # TODO - form validation
     con = engine.connect()
     if request.method == 'POST':
@@ -61,25 +57,6 @@ def machines():
     # return json here?
     return redirect(url_for('show_entries'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
     app.run()
