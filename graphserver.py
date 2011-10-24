a53249800c787e17d18d1677a177eff3eae59400
Lukas Blakk <lukasblakk@gmail.com>
2011-10-24 12:31:32 -0700

    currently broken in-browser, tests passing for webapp and API calls

--- graphserver.py
+++ graphserver.py
@@ -1,6 +1,6 @@
 import sqlite3
 import time
-from flask import Flask, request, session, redirect, url_for, jsonify
+from flask import Flask, request, redirect, url_for, jsonify
 from flask import abort, render_template, flash
 from contextlib import closing
 from sqlalchemy import create_engine, MetaData, Table
@@ -14,10 +14,16 @@ def init_db():
     app.con = app.engine.connect()
     app.branches = Table('branches', app.metadata, autoload=True)
     app.machines = Table('machines', app.metadata, autoload=True)
-    
+
+def is_json():
+    if 'application/json' in request.headers.get('Accept', ''):
+        return True
+    if request.args.get('format') == 'json':
+        return True
+    return False
+ 
 @app.before_request
 def before_request():
-    session['version'] = app.config['VERSION']
     init_db()
 
 @app.teardown_request
@@ -28,7 +34,9 @@ def teardown_request(exception):
 def show_entries():
     branch_list = app.engine.execute('select * from branches')
     machine_list = app.engine.execute('select * from machines')
-    return render_template('show_entries.html', branch_list=branch_list, machine_list=machine_list)
+    version = app.config['VERSION']
+    return render_template('show_entries.html', branch_list=branch_list, 
+                            machine_list=machine_list, version=version)
 
 @app.route('/branches', methods=['GET', 'POST'])
 def branches():
@@ -38,7 +46,7 @@ def branches():
             if exists.returns_rows:
                 results = app.con.execute(app.branches.delete().where(app.branches.c.id == request.form['id']))
                 flash('Branch %s was successfully deleted' % request.form['branch_name'])
-        elif request.form['_method'] == "insert":
+        else:
             exists = app.con.execute(app.branches.select().where(app.branches.c.name == request.form['branch_name']))
             if exists.fetchone() != None:
                 flash('Branch name exists, please enter a unique name')
@@ -47,7 +55,8 @@ def branches():
             else:
                 results = app.con.execute(app.branches.insert(), name=request.form['branch_name'])
                 flash('New branch %s was successfully added' % request.form['branch_name'])
-    if request.method == 'GET' and request.args.get('format') == 'json' or 'application/json' in request.headers.get('Accept', ''):
+# take this and the one for machines out into their own function for json? and then separate GET for each of these two
+    if is_json():
         return jsonify(app.branches.select().execute().fetchall())
     return redirect(url_for('show_entries'))
 
@@ -59,7 +68,7 @@ def machines():
             if exists.returns_rows:
                 results = app.con.execute(app.machines.delete().where(app.machines.c.id == request.form['id']))
                 flash("Machine '%s' was successfully deleted" % request.form['machine_name'])
-        elif request.form['_method'] == "insert":
+        else:
             errors = False
             for key,value in request.form.items():
                 if key == 'machine_name':
@@ -87,7 +96,7 @@ def machines():
                             date_added=int(time.time())
                         )
                 flash('New machine %s was successfully added' % request.form['machine_name'])
-    if request.method == 'GET' and request.args.get('format') == 'json' or 'application/json' in request.headers.get('Accept', ''):
+    if is_json():
         machines = {}
         results = app.machines.select().execute().fetchall()
         for r in results:
