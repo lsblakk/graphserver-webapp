c164df4d6f9a922231e94bc4c3bf4c4fd5f18869
Lukas Blakk <lukasblakk@gmail.com>
2012-02-08 15:25:31 -1000

    cleaned up, removed delete, updated tests, added Machine class

--- graphserver.py
+++ graphserver.py
@@ -33,10 +33,6 @@ def is_json():
 def before_request():
     init_db()
 
-@app.teardown_request
-def teardown_request(exception):
-    pass
-
 @app.route('/')
 def show_entries():
     branch_list = app.engine.execute('select * from branches')
@@ -50,26 +46,31 @@ def update_branch():
     if request.form.get('branch_id'):
         exists = app.con.execute(app.branches.select().where(app.branches.c.id == request.form['branch_id']))
         if exists.fetchone() != None:
-            # This branch exists, update the info
-            print "Branch exists"
+            # This branch exists, update the name
             branch_name, id = request.form.listvalues()
-            results = app.con.execute(app.branches.update().where(app.branches.c.id==int(id[0])).values(name=branch_name[0]))
-            flash('Branch %s updated' % request.form['branch_id'])
-            app.logger.info('Branch %s updated' % request.form['branch_id'])
+            # Validate user input
+            if len(branch_name) != 0:
+                results = app.con.execute(app.branches.update().where(app.branches.c.id==int(id[0])).values(
+                    name=branch_name[0]))
+                flash('Branch %s updated' % request.form['branch_id'])
+                app.logger.info('Branch %s updated' % request.form['branch_id'])
+            else:
+                flash('Branch name cannot be blank, not updated')
+                app.logger.info('Branch name cannot be blank, not updated')
     else:
-        # New branch to be inserted
-        print "New branch to be inserted"
-        results = app.con.execute(app.branches.insert(), name=request.form['branch_name'])
-        flash('New branch "%s" was successfully added' % request.form['branch_name'])
-        app.logger.info('New branch "%s" was successfully added' % request.form['branch_name'])
+        # New branch to try and insert - first validate input
+        if len(request.form.get('branch_name')) == 0:
+            flash('Error: Cannot add empty branch name')
+            app.logger.error('Cannot add empty branch name')
+        else:
+            results = app.con.execute(app.branches.insert(), name=request.form['branch_name'])
+            flash('New branch "%s" was successfully added' % request.form['branch_name'])
+            app.logger.info('New branch "%s" was successfully added' % request.form['branch_name'])
     
-    if is_json():
-        return jsonify(app.branches.select().execute().fetchall())
     return redirect(url_for('show_entries'))
 
-@app.route('/branch/<branch_id>?<branch_name>', methods=['POST'])
+@app.route('/branch/<branch_id>&branch_name=<branch_name>', methods=['GET'])
 def edit_branch(branch_id, branch_name):
-    print branch_name
     return render_template('update_entry.html', branch_id=branch_id, branch_name=branch_name)
 
 @app.route('/branches', methods=['GET'])
@@ -78,65 +79,57 @@ def get_branches():
         return jsonify(app.branches.select().execute().fetchall())
     return redirect(url_for('show_entries'))
 
-@app.route('/machines/<machine_id>?<machine_name>?<os_id>?<is_throttling>?<is_active>', methods=['POST'])
-def edit_machine(machine_id, machine_name, os_id, is_throttling, is_active):
-    print machine_name
-    return render_template('update_entry.html', machine_id=machine_id, machine_name=machine_name, os_id=os_id, is_throttling=is_throttling, is_active=is_active)
+@app.route('/machines/<machine_id>', methods=['POST'])
+def edit_machine(machine_id):
+    return render_template('update_entry.html', machine_id=machine_id,
+        name=request.form.get('name'),
+        os_id=request.form.get('os_id'),
+        is_throttling=request.form.get('is_throttling'),
+        is_active=request.form.get('is_active'),
+        cpu_speed=request.form.get('cpu_speed'))
 
 @app.route('/machines', methods=['POST'])
 def update_machines():
+    errors = False
+    # Existing machine has and id
     if request.form.get('machine_id'):
         exists = app.con.execute(app.machines.select().where(app.machines.c.id == request.form['machine_id']))
-        errors = False
-        if exists.fetchone() != None:
-            print "Machine exists"
-            print request.form.items()
-            results = app.con.execute(
-                app.machines.update().where(app.machines.c.id==int(request.form.get('machine_id'))).values(
-                name=request.form.get('machine_name'),
-                os_id=int(request.form.get('os_id')),
-                is_throttling=int(request.form.get('is_throttling')),
-                is_active=int(request.form.get('is_active')),
-            ))
-            flash('Machine %s updated' % request.form['machine_id'])
-            app.logger.info('Machine %s updated' % request.form['machine_id'])
+        entry = exists.fetchone()
+        if entry != None:            
+            machine = Machine(id=request.form['machine_id'],name=request.form.get('machine_name'),
+                os_id=request.form.get('os_id'),is_throttling=request.form.get('is_throttling'),
+                is_active=request.form.get('is_active'),cpu_speed=request.form.get('cpu_speed'))
+            if machine.valid:
+                print "Machine is valid %s" % machine
+                results = app.con.execute(app.machines.update().where(app.machines.c.id==int(request.form.get('machine_id'))).values(machine.toDict()))
+                flash('Machine %s updated' % request.form['machine_id'])
+                app.logger.info('Machine %s updated' % request.form['machine_id'])
+            else:
+                flash('Machine cannot be updated, some values are incorrect')
+                app.logger.info('Machine cannot be updated, some values are incorrect')
+    # Otherwise we are going to insert a new machine
     else:
-        for key,value in request.form.items():
-            if key == 'machine_name':
-                exists = app.con.execute(app.machines.select().where(app.machines.c.name == request.form['machine_name']))
-                if exists.fetchone() != None:
-                    flash('Machine name "%s" exists, please enter a unique name' % request.form['machine_name'])
-                    app.logger.warning('Machine name "%s" exists, please enter a unique name', request.form['machine_name'])
-                    errors = True
-                elif value == '':
-                    flash('Machine name cannot be blank')
-                    app.logger.warning('Machine name cannot be blank')
-                    errors = True
-            elif key in ('os_id', 'is_throttling', 'cpu_speed', 'is_active'):
-                try:
-                    i = float(value)
-                except ValueError, TypeError:
-                    flash('"%s" must be a numeric value' % key)
-                    app.logger.warning('"%s" must be a numeric value' % key)
-                    errors = True
-        if not errors:
+        exists = app.con.execute(app.machines.select().where(app.machines.c.name == 
+                    request.form['machine_name']))
+        if exists.fetchone() != None:
+            flash('Machine name "%s" exists, please enter a unique name' % request.form['machine_name'])
+            app.logger.warning('Machine name "%s" exists, please enter a unique name', request.form['machine_name'])
+            errors = True
+        machine = Machine(name=request.form['machine_name'], is_throttling=request.form['is_throttling'],
+            is_active=request.form['is_active'], cpu_speed=request.form['cpu_speed'], os_id = request.form['os_id'])
+        if not errors and machine.valid:
+            insert_values = machine.toDict()
+            insert_values['date_added'] = int(time.time())
             results = app.con.execute(
                         app.machines.insert(), 
-                        os_id=int(request.form['os_id']),
-                        is_throttling=int(request.form['is_throttling']),
-                        cpu_speed=request.form['cpu_speed'], 
-                        name=request.form['machine_name'], 
-                        is_active=int(request.form['is_active']), 
-                        date_added=int(time.time())
+                        insert_values,
                     )
             flash('New machine "%s" was successfully added' % request.form['machine_name'])
             app.logger.info('New machine "%s" was successfully added' % request.form['machine_name'])
-    if is_json():
-        machines = {}
-        results = app.machines.select().execute().fetchall()
-        for r in results:
-            machines[r[0]] = r[4]
-        return jsonify(machines)
+        elif not machine.valid:
+            flash('Check machine values (name, cpu_speed, os_id) for correctness, no machine added.')
+            app.logger.info('Check machine values (name, cpu_speed, os_id) for correctness, no machine added.')
+
     return redirect(url_for('show_entries'))  
 
 @app.route('/machines', methods=['GET'])
@@ -149,5 +142,59 @@ def get_machines():
         return jsonify(machines)
     return redirect(url_for('show_entries'))    
 
+class Machine(object):
+    # Defaults: is_throttling = 0, is_active = 1 
+    # If name or cpu_speed don't validate then valid is False
+    def __init__(self, id=None, os_id=False, is_throttling=False, cpu_speed=False,
+            name=False, is_active=False, valid=False):
+            self.id = id
+            if self.id != None:
+                # fetch the existing machine
+                exists = app.con.execute(app.machines.select().where(app.machines.c.id == id))
+                machine = exists.fetchone()
+                if machine != 0:
+                    self.os_id = machine[1]
+                    self.is_throttling = machine[2]
+                    self.cpu_speed = machine[3]
+                    self.name = machine[4]
+                    self.is_active = machine[5]
+                    self.valid = True
+            else:   
+                try:
+                    self.os_id = int(os_id)
+                except:
+                    self.os_id = False
+                if is_throttling in [0,1]:
+                    self.is_throttling = is_throttling
+                else:
+                    self.is_throttling = 0
+                try:
+                    self.cpu_speed = float(cpu_speed)
+                except:
+                    self.cpu_speed = False
+                self.name = name
+                if is_active in [0,1]:
+                    self.is_active = is_active
+                else:
+                    self.is_active = 1
+                if len(self.name) != 0 and self.cpu_speed and self.os_id:
+                    self.valid = True
+                else:
+                    self.valid = valid
+    
+    def __repr__(self):
+        return str(self.toDict())
+    
+    def toDict(self):
+        d = {
+            'id': self.id,
+            'os_id': self.os_id,
+            'is_throttling': self.is_throttling,
+            'is_active': self.is_active,
+            'cpu_speed': self.cpu_speed,
+            'name': self.name,
+        }
+        return d
+        
 if __name__ == '__main__':
     app.run()
--- test/test_graphserver.py
+++ test/test_graphserver.py
@@ -4,59 +4,70 @@ import graphserver
 import unittest
 import tempfile
 import json
+import subprocess
 
 class GraphserverTestCase(unittest.TestCase):
 
     def setUp(self):
         graphserver.app.config.from_object('config.TestConfig')
         self.app = graphserver.app.test_client()
+        # Remove the db and recreate it
+        if os.path.exists('test_graphserver.sqlite'):
+            os.remove('test_graphserver.sqlite')
+            subprocess.call("sqlite3 test_graphserver.sqlite < test_db.sql", shell=True)
+        else:
+            subprocess.call("sqlite3 test_graphserver.sqlite < test_db.sql", shell=True)
 
     def test_empty_db(self):
+        # Remove the db and recreate it
+        if os.path.exists('test_graphserver.sqlite'):
+            os.remove('test_graphserver.sqlite')
+            subprocess.call("sqlite3 test_graphserver.sqlite < test_db.sql", shell=True)
+        else:
+            subprocess.call("sqlite3 test_graphserver.sqlite < test_db.sql", shell=True)
         rv = self.app.get('/')
+        print rv.data
         assert 'No machines' in rv.data
         assert 'No branches' in rv.data
 
     def test_add_empty_branch_web(self):
-        rv = self.app.post('/branches', data=dict(
+        rv = self.app.post('/branch', data=dict(
             branch_name='',
         ), follow_redirects=True)
-        assert 'cannot be blank' in rv.data
+        assert 'Cannot add empty branch name' in rv.data
 
     def test_add_branch(self):
-        rv = self.app.post('/branches', data=dict(
+        rv = self.app.post('/branch', data=dict(
             branch_name='new_branch',
-            _method='insert'
         ), follow_redirects=True)
         assert 'new_branch' in rv.data
         resp = self.app.get('/branches?format=json', follow_redirects=True)
         results = json.loads(resp.data)
         assert results['1'] == 'new_branch'
 
-    def test_delete_branch(self):
-        rv = self.app.post('/branches', data=dict(
-            id=1,
-            branch_name='new_branch',
-            _method='delete'
-        ), follow_redirects=True)
+    def test_update_branch(self):
         rv = self.app.get('/')
-        assert 'No branches' in rv.data
-
-    def test_delete_nonexistent_branch_json(self):
-        resp = self.app.get('/branches?format=json', follow_redirects=True)
-        results_pre = json.loads(resp.data)
-        rv = self.app.post('/branches', data=dict(
-            id=14,
-            format='json',
-            _method='delete'
-        ), follow_redirects=True)
-        resp = self.app.get('/branches?format=json', follow_redirects=True)
-        results_post = json.loads(resp.data)
-        assert results_pre == results_post
+        if 'No branches' in rv.data:
+            print  "Need to add the branch first"
+            rv = self.app.post('/branch', data=dict(
+                branch_name='new_branch',
+            ), follow_redirects=True)
+        rv = self.app.get('/branches?format=json')
+        results = json.loads(rv.data)
+        print results
+        rv = self.app.post('/branch', data=dict(
+            branch_id=1,
+            branch_name='updated_branch'),
+            follow_redirects=True)
+        rv = self.app.get('/branches?format=json')
+        results = json.loads(rv.data)
+        print results
+        assert results['1'] == 'updated_branch'
 
     def test_add_empty_branch_json(self):
         resp = self.app.get('/branches?format=json', follow_redirects=True)
         results_pre = json.loads(resp.data)
-        rv = self.app.post('/branches', data=dict(
+        rv = self.app.post('/branch', data=dict(
             branch_name='',
             format='json',
         ), follow_redirects=True)
@@ -107,26 +118,24 @@ class GraphserverTestCase(unittest.TestCase):
         results_post = json.loads(resp.data)
         assert results_pre == results_post
 
-    def test_delete_nonexistent_machine_json(self):
-        resp = self.app.get('/machines?format=json', follow_redirects=True)
-        results_pre = json.loads(resp.data)
+    def test_update_machine(self):
+        resp = self.app.get('/', follow_redirects=True)
+        if 'No machines' in resp.data:
+            print "Need to insert a machine"
+            rv = self.app.post('/machines', data=dict(
+                os_id=13,
+                is_throttling=1,
+                cpu_speed=1.12,
+                machine_name='new_machine',
+                is_active=0,
+            ), follow_redirects=True)
         rv = self.app.post('/machines', data=dict(
-            id=14,
-            format='json',
-            _method='delete'
+            machine_id=1,
+            machine_name='updated_machine',
         ), follow_redirects=True)
         resp = self.app.get('/machines?format=json', follow_redirects=True)
-        results_post = json.loads(resp.data)
-        assert results_pre == results_post
-
-    def test_delete_machine(self):
-        rv = self.app.post('/machines', data=dict(
-            id=1,
-            machine_name='new_machine',
-            _method='delete'
-        ), follow_redirects=True)
-        rv = self.app.get('/')
-        assert 'No machines' in rv.data
-
+        results = json.loads(resp.data)
+        assert results['1'] == 'new_machine'
+    
 if __name__ == '__main__':
     unittest.main()
